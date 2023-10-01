2efb91854002daaa1f7f85edbd525d0008b59fea
roshan5977 <roshannayak5977@gmail.com>
2023-10-01 18:11:21 +0530

    bubble

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,3 +1,5 @@
+# bubble 
+
 def sort(arr_to_sort):
     for i in range(0,len(arr_to_sort)):
         for j in range(0,len(arr_to_sort)-i-1):
@@ -7,3 +9,4 @@ def sort(arr_to_sort):
 
 arr=[2,4,8,2,5,9,8,4,2,7,9,3,8,3,7,0,7,6,4,6,8,1,3,8,5,4,4,6,7,9,8]
 print(sort(arr))
+
